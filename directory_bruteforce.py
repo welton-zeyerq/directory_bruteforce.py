b70c72cefdaa34e67d68fa292c20bd054c4928a6
welton-zeyerq <106247800+welton-zeyerq@users.noreply.github.com>
2022-11-06 00:53:05 +0000

    Update directory_bruteforce.py

--- directory_bruteforce.py
+++ directory_bruteforce.py
@@ -14,46 +14,45 @@ except:
 if len(sys.argv) !=4:
     print("follow the example: ")
     print("")
-    print("%s https://www.site.com /usr/share/wordlists/wfuzz/general/common.txt /home/user/Documents/directory.txt"%(sys.argv[0]))
+    print("%s https://link.com /usr/share/wordlists/wfuzz/general/common.txt /home/ghost/Documents/directory.txt"%(sys.argv[0]))
     sys.exit()
 
 LOG = sys.argv[3]
 logging.basicConfig(level=logging.INFO, filename=LOG, format="%(message)s")
 
 def brute(url, wordlist):
-    while True:
-        for word in wordlist:
-            try:
-                url_final = "{}/{}".format(url, word.strip())
-                print(url_final)
-                response = requests.get(url_final)
-                code = response.status_code
-                if code != 404:
-                    logging.info("{} [code {}]".format(url_final, code))
-                    print("{} [code {}]".format(url_final, code))
-                else:
-                    logging.info("{} [code {}]".format(url_final, code))
-                    print("{} [code {}]".format(url_final, code))
-            except KeyboardInterrupt:
-                sys.exit()
-            except Exception as error:
-                print(error)
-                pass
-        else:
+    for word in wordlist:
+        try:
+            url_final = "{}/{}".format(url, word.strip())
+            print(url_final)
+            response = requests.get(url_final)
+            code = response.status_code
+            if code != 404:
+                logging.info("{} [code {}]".format(url_final, code))
+                print("{} [code {}]".format(url_final, code))
+            else:
+                logging.info("{} [code {}]".format(url_final, code))
+                print("{} [code {}]".format(url_final, code))
+        except KeyboardInterrupt:
+            sys.exit()
+        except Exception as error:
+            print(error)
             pass
+    else:
+        pass
 
     try:
-        THREADS = []
-        for i in range(8):
-            t = threading.Thread(target=brute)
-            THREADS.append(t)
-        for t in THREADS:
-            t.start()
-            t.join()
+        while True:
+            if _ in range(4):
+                threading.Thread(target=brute).join.start()
+            else:
+                print("threading error")
+                pass
+        else:
+            print("error")
     except KeyboardInterrupt:
         sys.exit()
     except Exception as error:
-        print(error)
         pass
 
 try:
